6074dc5d48382d3bbb6623dbe1f153081f0d541e
Miguel Lucendo <miguel.lucendo.esteban@gmail.com>
2023-01-13 00:39:11 +0100

    Funcionamiento tabla usuario con ajax

--- view/admin/js/listadoUsuario.js
+++ view/admin/js/listadoUsuario.js
@@ -1,14 +1,73 @@
 let URL_BASE = 'http://localhost/christies-and-meta/index.php/api/';
+var paginaActual;
 
 window.onload = () => {
     // Carga dinamicamente la primera pagina
+    paginaActual = 1;
     let url = URL_BASE + 'usuarios/1';
 
     $.ajax({
         url: url,
         type: 'GET',
         success: (json) => {
-            console.log(json);
+            let usuarios = JSON.parse(json);
+            let tbody = document.querySelector('#tbody-usuario');
+            usuarios.forEach(usuario => {
+                let fila = document.createElement('tr');
+                fila.innerHTML = `<td>${usuario.codUsuario}</td><td>${usuario.email}</td><td>${usuario.nombre}</td>`+
+                `<td>${usuario.apellidos}</td><td>${usuario.moneda}</td>`;
+                tbody.appendChild(fila);
+            });
+        }
+    })
+}
+
+function siguiente() {
+    let url = URL_BASE + 'usuarios/'+ ++paginaActual;
+
+    $.ajax({
+        url: url,
+        type: 'GET',
+        success: (json) => {
+            let usuarios = JSON.parse(json);
+            let tbody = document.querySelector('#tbody-usuario');
+            if (usuarios.length > 0) {
+                tbody.innerHTML = '';
+                usuarios.forEach(usuario => {
+                        let fila = document.createElement('tr');
+                        fila.innerHTML = `<td>${usuario.codUsuario}</td><td>${usuario.email}</td><td>${usuario.nombre}</td>`+
+                        `<td>${usuario.apellidos}</td><td>${usuario.moneda}</td>`;
+                        tbody.appendChild(fila);
+                });
+            } else {
+                paginaActual--;
+            }
+        }
+    })
+}
+function anterior() {
+    let url = URL_BASE + 'usuarios/'+ --paginaActual;
+    if (paginaActual < 1) {
+        paginaActual = 1;
+        return;
+    }
+    $.ajax({
+        url: url,
+        type: 'GET',
+        success: (json) => {
+            let usuarios = JSON.parse(json);
+            if (usuarios.length > 0) {
+                let tbody = document.querySelector('#tbody-usuario');
+                tbody.innerHTML = '';
+                usuarios.forEach(usuario => {
+                    let fila = document.createElement('tr');
+                    fila.innerHTML = `<td>${usuario.codUsuario}</td><td>${usuario.email}</td><td>${usuario.nombre}</td>`+
+                    `<td>${usuario.apellidos}</td><td>${usuario.moneda}</td>`;
+                    tbody.appendChild(fila);
+                });
+            } else {
+                paginaActual++;
+            }
         }
     })
 }
--- view/admin/usuarios.php
+++ view/admin/usuarios.php
@@ -7,6 +7,7 @@ $project_url = $decoded_json['project_url'];
 <html lang="en">
 
 <head>
+<script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
     <base href="<?php echo $project_url ?>">
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -77,73 +78,14 @@ $project_url = $decoded_json['project_url'];
                                                     <th>Moneda</th>
                                                 </tr>
                                             </thead>
-                                            <tbody>
-                                                <tr>
-                                                    <td>Jacob</td>
-                                                    <td>Photoshop</td>
-                                                    <td class="text-danger"> 28.76% <i class="mdi mdi-arrow-down"></i></td>
-                                                    <td><label class="badge badge-danger">Pending</label></td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Messsy</td>
-                                                    <td>Flash</td>
-                                                    <td class="text-danger"> 21.06% <i class="mdi mdi-arrow-down"></i></td>
-                                                    <td><label class="badge badge-warning">In progress</label></td>
-                                                </tr>
-                                                <tr>
-                                                    <td>John</td>
-                                                    <td>Premier</td>
-                                                    <td class="text-danger"> 35.00% <i class="mdi mdi-arrow-down"></i></td>
-                                                    <td><label class="badge badge-info">Fixed</label></td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Peter</td>
-                                                    <td>After effects</td>
-                                                    <td class="text-success"> 82.00% <i class="mdi mdi-arrow-up"></i></td>
-                                                    <td><label class="badge badge-success">Completed</label></td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Dave</td>
-                                                    <td>53275535</td>
-                                                    <td class="text-success"> 98.05% <i class="mdi mdi-arrow-up"></i></td>
-                                                    <td><label class="badge badge-warning">In progress</label></td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Dave</td>
-                                                    <td>53275535</td>
-                                                    <td class="text-success"> 98.05% <i class="mdi mdi-arrow-up"></i></td>
-                                                    <td><label class="badge badge-warning">In progress</label></td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Dave</td>
-                                                    <td>53275535</td>
-                                                    <td class="text-success"> 98.05% <i class="mdi mdi-arrow-up"></i></td>
-                                                    <td><label class="badge badge-warning">In progress</label></td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Dave</td>
-                                                    <td>53275535</td>
-                                                    <td class="text-success"> 98.05% <i class="mdi mdi-arrow-up"></i></td>
-                                                    <td><label class="badge badge-warning">In progress</label></td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Dave</td>
-                                                    <td>53275535</td>
-                                                    <td class="text-success"> 98.05% <i class="mdi mdi-arrow-up"></i></td>
-                                                    <td><label class="badge badge-warning">In progress</label></td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Dave</td>
-                                                    <td>53275535</td>
-                                                    <td class="text-success"> 98.05% <i class="mdi mdi-arrow-up"></i></td>
-                                                    <td><label class="badge badge-warning">In progress</label></td>
-                                                </tr>
+                                            <tbody id="tbody-usuario">
+                                                
                                             </tbody>
                                         </table>
                                     </div>
                                     <div class="btn-group" role="group" aria-label="Basic example">
-                                        <button type="button" class="btn btn-outline-info">Anterior</button>
-                                        <button type="button" class="btn btn-outline-info">Siguiente</button>
+                                        <button type="button" class="btn btn-outline-info" onclick="anterior()">Anterior</button>
+                                        <button type="button" class="btn btn-outline-info" onclick="siguiente()">Siguiente</button>
                                     </div>
                                 </div>
                             </div>
@@ -185,7 +127,7 @@ $project_url = $decoded_json['project_url'];
     <!-- Custom js for this page -->
     <!-- End custom js for this page -->
     <!-- MIS JS -->
-    <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+    
     <script src="view/admin/js/listadoUsuario.js"></script>
 </body>
 
