51604b97eb4e136a78d91a1248b447fa5e365e1d
Miguel Lucendo <miguel.lucendo.esteban@gmail.com>
2023-01-26 11:39:53 +0100

    Securizado backend

--- controller/Controller.php
+++ controller/Controller.php
@@ -14,15 +14,19 @@ class Controller {
         include 'view/admin/fichaejemplo.php';
     }
     public function muestraUsuarios() {
+        include 'model/controlback.php';
         include 'view/admin/usuarios.php';
     }
     public function muestraProductos() {
+        include 'model/controlback.php';
         include 'view/admin/productos.php';
     }
     public function muestraCategorias() {
+        include 'model/controlback.php';
         include 'view/admin/categorias.php';
     }
     public function muestraComentarios() {
+        include 'model/controlback.php';
         include 'view/admin/comentarios.php';
     }    
     /**
@@ -32,10 +36,12 @@ class Controller {
      * @return void
      */
     public function muestraFichaUsuario(int $codUsuario) {
+        include 'model/controlback.php';
         $usuario = Usuario::getUsuarioByCod($codUsuario);
         include 'view/admin/fichausuario.php';
     }
     public function muestraFichaCategoria(int $codCategoria) {
+        include 'model/controlback.php';
         $categorias = Categoria::getAllCategorias();
         $categoria = Categoria::getCategoriaByCod($codCategoria);
         if ($categoria->codCategoriaPadre) {
@@ -44,6 +50,7 @@ class Controller {
         include 'view/admin/fichacategoria.php';
     }
     public function muestraFichaCategoriaNueva() {
+        include 'model/controlback.php';
         $categorias = Categoria::getAllCategorias();
         $categoria = new Categoria(
             0, '', '', '', 0
@@ -51,6 +58,7 @@ class Controller {
         include 'view/admin/fichacategoria.php';
     }
     public function muestraFichaProducto(int $codProducto) {
+        include 'model/controlback.php';
         $producto = Producto::getProductoByCod($codProducto);
         $categorias = Categoria::getAllCategorias();
         include 'view/admin/fichaproducto.php';
